3acc39a3e9abe71b068455fe43826592344cfc74
Mahmoud elqenawey <mahmoudelqenawey1@gmail.com>
2025-03-01 10:41:31 +0200

    add chakra createSystem

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,15 +1,33 @@
-"use client"
+"use client";
 
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
-import {
-  ColorModeProvider,
-  type ColorModeProviderProps,
-} from "./color-mode"
+import { ChakraProvider, createSystem, defaultConfig } from "@chakra-ui/react";
+import type { PropsWithChildren } from "react";
+import { ColorModeProvider } from "./color-mode";
 
-export function Provider(props: ColorModeProviderProps) {
-  return (
-    <ChakraProvider value={defaultSystem}>
-      <ColorModeProvider {...props} />
-    </ChakraProvider>
-  )
-}
+const system = createSystem(defaultConfig, {
+  // globalCss: {
+  //   body: {
+  //     colorPalette: "teal",
+  //   },
+  // },
+  // theme: {
+  //   tokens: {
+  //     fonts: {
+  //       body: { value: "var(--font-outfit)" },
+  //     },
+  //   },
+  //   semanticTokens: {
+  //     radii: {
+  //       l1: { value: "0.125rem" },
+  //       l2: { value: "0.25rem" },
+  //       l3: { value: "0.375rem" },
+  //     },
+  //   },
+  // },
+});
+
+export const Provider = (props: PropsWithChildren) => (
+  <ChakraProvider value={system}>
+    <ColorModeProvider>{props.children}</ColorModeProvider>
+  </ChakraProvider>
+);
